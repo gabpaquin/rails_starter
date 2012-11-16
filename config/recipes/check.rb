54a1357c213c46783e749b098b3de9486d032c67
Gabriel Paquin <gpaquin@webagecorp.com>
2012-11-16 12:02:58 +0800

    deploy script change check revision to origin/master

--- config/recipes/check.rb
+++ config/recipes/check.rb
@@ -1,8 +1,8 @@
 namespace :check do
   desc "Make sure local git is in sync with remote."
   task :revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse master/#{branch}`
-      puts "WARNING: HEAD is not the same as master/#{branch}"
+    unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
+      puts "WARNING: HEAD is not the same as origin/#{branch}"
       puts "Run `git push` to sync changes."
       exit
     end
